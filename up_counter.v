007d546ca6353c14727aa6887181c691065771e9
Priyanshu Sinha <officialpriyanshusinha@gmail.com>
2023-08-07 10:12:16 +0530

    Update up_counter.v

--- up_counter.v
+++ up_counter.v
@@ -5,10 +5,10 @@
         input clk, reset_n,enable, 
         output[counter_bits-1:0] counter_out
     );
-        //seq. style
+
         reg [counter_bits-1:0] q_reg, q_next;
 
-        //seq part
+
         always @(posedge clk, negedge reset_n) begin
             if (~reset_n)
                 q_reg <= 'd0;
@@ -20,12 +20,12 @@
 
         end
         
-        //next state logic
+
         always @(*) begin
             q_next = q_reg + 1'b1; 
         end
 
-        // output logic
+
 
         assign counter_out = q_reg;
 
@@ -36,26 +36,24 @@
 
 module up_counter_tb();
 
-  // Port and parameter declaration
+
   localparam counter_bits = 7;
   localparam period = 5;
 
   reg clk, reset_n, enable;
   wire [counter_bits-1:0]counter_out;
 
-  // Instantiate the UUT module
+
    up_counter #(.counter_bits(counter_bits))UUT (.clk(clk), .reset_n(reset_n), .counter_out(counter_out), .enable(enable));
 
 
 
-  // Specify a stopwatch for the simulation
   initial begin
     #1000;
     $finish;
   end
 
-  // Generate stimuli using initial and always blocks
-  // Clock generation
+
   always begin
     clk = 1'b1;
     #period;
@@ -63,7 +61,6 @@ module up_counter_tb();
     #period;
   end
 
-  // Stimuli generation
   initial begin
     enable=1'b1;
     reset_n = 1'b0;
@@ -72,7 +69,6 @@ module up_counter_tb();
 
   end
 
-  // Monitor the output
   initial begin
     $monitor("time = %d\t  output = %d\t reset = %b", $time, counter_out, reset_n);
   end
